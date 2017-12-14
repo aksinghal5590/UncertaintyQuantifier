0715de00ecfbbea278628f326eb2719a8d58c2e2
Iti Inani <iti.inani@stonybrook.edu>
2017-12-14 05:41:47 -0500

    changed

--- src/error_addition.py
+++ src/error_addition.py
@@ -1,10 +1,12 @@
+import sys
+import predict_errorValue
 import pandas as pd
 import csv
 import statistics
 
 def error_addition(inputDir):
-    # predict_errorValue.predict_error_value(inputDir)
-    print("I am in")
+    predict_errorValue.predict_error_value(inputDir)
+    # print("I am in")
     df1 = pd.read_csv("../bin/testing_data_" + inputDir + ".csv", sep="\t")
     df2 = pd.read_csv("../bin/pred_errorValue_" + inputDir + ".csv", sep="\t")
     # print(df1["StandardDeviation"],df2["Predicted_ErrorValue"])
@@ -13,11 +15,11 @@ def error_addition(inputDir):
     i = 0
     for err in df2["Predicted_ErrorValue"]:
         if err < 0:
-            df2.ix[i,"Diff"] = df2.ix[i,"Mean"] - (df2.ix[i,"Predicted_ErrorValue"])*(0.45)
+            df2.ix[i,"Diff"] = df2.ix[i,"Mean"] - (df2.ix[i,"Predicted_ErrorValue"])*(0.5)
         if err > 0:
-            df2.ix[i,"Diff"] = df2.ix[i,"Mean"] + (df2.ix[i,"Predicted_ErrorValue"])*(0.45)
+            df2.ix[i,"Diff"] = df2.ix[i,"Mean"] + (df2.ix[i,"Predicted_ErrorValue"])*(0.5)
         i+=1
-    print(df2, df1["Variance"])
+    # print(df2, df1["Variance"])
     names = df2["Name"].tolist()
     # print(df2["Diff"])
 
@@ -48,6 +50,7 @@ def error_addition(inputDir):
         if data[0] in ciMap:
             ciTuple = ciMap[data[0]]
             if (float(data[1]) < ciTuple[0]) or (float(data[1]) > ciTuple[1]):
+                # print(data[0],data[1],ciTuple[0],ciTuple[1])
                 tuple = data[0]
                 faultyTr.append(tuple)
 
@@ -55,13 +58,13 @@ def error_addition(inputDir):
 
     data_frame = pd.read_csv('../input/' + inputDir + '/quant_bootstraps.tsv', sep='\t')
     new_names = list(data_frame.columns.values)
+    # i = 0
     for name in new_names:
         if name in names:
             loc = names.index(name)
-            data_frame[name] += df2.ix[loc,"Diff1"]
+            data_frame[name] += (df2.ix[loc,"Predicted_ErrorValue"])
     data_frame.to_csv('../input/' + inputDir + '/quant_bootstraps_new.tsv', sep='\t',index=False)
     print("Done")
 
 
-print("Hello")
-error_addition("poly_mo")
+error_addition(sys.argv[1])
